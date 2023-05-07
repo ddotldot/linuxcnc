cc1b1522a206e5cd76da1fd2e04d31d7016ea4ca
D.L <94646173+ddotldot@users.noreply.github.com>
2023-05-07 19:04:16 +0300

    small fix axis.py

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -2197,9 +2197,6 @@ class TclCommands(nf.TclCommands):
         s.poll()
         if s.task_state == linuxcnc.STATE_ESTOP_RESET:
             c.state(linuxcnc.STATE_ON)
-            runHoming=prompt_areyousure(_("Homing request"),_("After turning on the machine power,\nYou need find axes origins.\n\n            Run homing process?"))
-            if runHoming:
-                commands.home_all_joints()
         else:
             c.state(linuxcnc.STATE_OFF)
 
